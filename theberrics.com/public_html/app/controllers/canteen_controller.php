a3c329280c7afeddd54491d160d011949bacdf00
John Hardy <john.hardy@me.com>
2011-09-13 19:01:09 -0700

    START WORK ON CATEGORY CODE

--- theberrics.com/public_html/app/controllers/canteen_controller.php
+++ theberrics.com/public_html/app/controllers/canteen_controller.php
@@ -45,17 +45,38 @@ class CanteenController extends CanteenAppController {
 	public function category() {
 		
 		$this->loadModel("CanteenCategory");
+		$this->loadModel("CanteenProduct");
 		
 		$uri = $this->params['uri'];
 		
-		$category = $this->CanteenCategory->find("first",array(
+		$cat_token = "canteen_cat_".md5($uri);
 		
-			"conditions"=>array("CanteenCategory.uri"=>$uri,"CanteenCategory.active"=>1)
-		
-		));
-		
+		if(($category = Cache::read($cat_token,"1min")) === false) {
+
+			$category = $this->CanteenCategory->find("first",array(
+			
+				"conditions"=>array("CanteenCategory.uri"=>$uri,"CanteenCategory.active"=>1),
+				"contain"=>array()
+			
+			));
+
+			Cache::write($cat_token,$category,"1min");
+			
+		}
+
 		$this->set(compact("category"));
 		
+		$prod_ids = $this->CanteenProduct->find("all",array(
+			"fields"=>array("CanteenProduct.id"),
+			"conditions"=>array(
+				"CanteenProduct.canteen_category_id"=>$category['CanteenCategory']['id']
+			),
+			"contain"=>array()
+		));
+		
+		$prod_ids = Set::extract("/CanteenProduct/id",$prod_ids);
+		
+		//die(print_r($prod_ids));
 		
 	}
 	
