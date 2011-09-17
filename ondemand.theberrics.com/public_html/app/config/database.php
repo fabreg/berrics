3288b1ec840944ac5ea9c6a1d98a54befb856622
John Hardy <john.hardy@me.com>
2011-09-16 17:22:32 -0700

    ADD CORE CSS AND CONFIG FILES FOR ONDEMAND DOMAIN
    - Update database.php to use smart constructor
    - Update paths in bootstrap to detect virtual machine
    - Create core css files for layout

--- ondemand.theberrics.com/public_html/app/config/database.php
+++ ondemand.theberrics.com/public_html/app/config/database.php
@@ -1,17 +1,16 @@
 <?php
-
 class DATABASE_CONFIG {
 	
 	public $default = array(
 		'driver' => 'mysql',
 		'persistent' => false,
-		'host' => '10.181.78.75',
+		'host' => '10.181.66.225',
 		'login' => 'john',
 		'password' => '19Berrics82',
 		'database' => 'theberrics',
 		'prefix' => '',
-	);
-
+	); 
+	
 	public $master = array(
 		'driver' => 'mysql',
 		'persistent' => false,
@@ -26,7 +25,7 @@ class DATABASE_CONFIG {
 	public $traffic = array(
 		'driver' => 'mysql',
 		'persistent' => false,
-		'host' => '10.181.91.233',
+		'host' => '10.181.80.72',
 		'login' => 'john',
 		'password' => '19Berrics82',
 		'database' => 'theberrics_traffic',
@@ -57,12 +56,32 @@ class DATABASE_CONFIG {
 	public $legacy = array(
 		'driver' => 'mysql',
 		'persistent' => false,
-		'host' => '10.181.66.225',
+		'host' => '10.181.91.233',
 		'login' => 'john',
 		'password' => '19Berrics82',
 		'database' => 'theberrics_legacy',
 		'prefix' => '',
 	);
 	
+	public function __construct() {
+
+		$uname = php_uname('n');
+
+		if($uname == 'WEB2VM.THEBERRICS') {
+			
+			$this->default['host'] = $this->master['host'] = '127.0.0.1';
+		}	
+
+	}
+
+
+	function DATABASE_CONFIG() 
+    { 
+        $this->__construct(); 
+    } 
+
+
+
 	
+
 }
