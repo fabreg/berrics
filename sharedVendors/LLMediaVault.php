20d285784329ace78ada5237a2cfbdcbc1e70ce3
John Hardy <john.hardy@me.com>
2011-09-19 18:20:08 -0700

    ADD CRYPTO HASHING TO LLMediaVault.php
    - Add methods to crytographically cypher llwn
      media vault urls to have a TTL in seconds

--- sharedVendors/LLMediaVault.php
+++ sharedVendors/LLMediaVault.php
@@ -5,6 +5,10 @@ class LLMediaVault {
 	
 	public static $instance = false;
 	
+	public $mv_hash = "yzJiEzNqFAfu6";
+	
+	public $mv_url = "http://berrics.vo.llnwd.net/o45/s/";
+	
 	private function __construct() {
 		
 		
@@ -23,6 +27,29 @@ class LLMediaVault {
 		
 	}
 	
+	
+	public function returnSecureUrl($MediaFile = array(),$ttl = 30) {
+		
+		if(
+			!isset($MediaFile['id']) || 
+			!isset($MediaFile['limelight_file']) ||
+			($MediaFile['limelight_mediavault_active']!=1)
+		) {
+			
+			throw new Exception("[LLMediaVault::returnSecureUrl()]:Invalid Media File");
+			
+		}
+		
+		//30 seconds in the future
+		$time = strtotime("+{$ttl} Seconds");
+		
+		$url = $this->mv_url.$MediaFile['limelight_file']."?e=".$time;
+		
+		$hash = md5($this->mv_hash.$url);
+		
+		return $url."&h=".$hash;
+		
+	}
 	
 	
 	
