8e8d71194c508cf3cda7f31ee48ef32dc575acf4
John Hardy <john.hardy@me.com>
2011-09-29 12:27:47 -0700

    FIX MEMORY LEAK IN NEWS
    - Overload pagination methods in dailyop
      model
    - Add cache models to both admin core
      and site core to support new pagination
      methods
    - Update cache paths

--- sharedModels/dailyop.php
+++ sharedModels/dailyop.php
@@ -768,6 +768,44 @@ class Dailyop extends AppModel {
 	}
 		
 	
+	function paginate ($conditions, $fields, $order, $limit, $page = 1, $recursive = null, $extra = array()) {
+		$args = func_get_args();
+		$uniqueCacheId = '';
+		foreach ($args as $arg) {
+			$uniqueCacheId .= serialize($arg);
+		}
+		if (!empty($extra['contain'])) {
+			$contain = $extra['contain'];
+		}
+		$uniqueCacheId = md5($uniqueCacheId);
+		$pagination = Cache::read('pagination-'.$this->alias.'-'.$uniqueCacheId, 'paginate_cache');
+		if (empty($pagination)) {
+			$pagination = $this->find('all', compact('conditions', 'fields', 'order', 'limit', 'page', 'recursive', 'group', 'contain'));
+			Cache::write('pagination-'.$this->alias.'-'.$uniqueCacheId, $pagination, 'paginate_cache');
+		}
+		return $pagination;
+	}
+
+	function paginateCount ($conditions = null, $recursive = 0, $extra = array()) {
+		$args = func_get_args();
+		$uniqueCacheId = '';
+		foreach ($args as $arg) {
+			$uniqueCacheId .= serialize($arg);
+		}
+		$uniqueCacheId = md5($uniqueCacheId);
+		if (!empty($extra['contain'])) {
+			$contain = $extra['contain'];
+		}
+
+		$paginationcount = Cache::read('paginationcount-'.$this->alias.'-'.$uniqueCacheId, 'paginate_cache');
+		if (empty($paginationcount)) {
+			$paginationcount = $this->find('count', compact('conditions', 'contain', 'recursive'));
+			Cache::write('paginationcount-'.$this->alias.'-'.$uniqueCacheId, $paginationcount, 'paginate_cache');
+		}
+		return $paginationcount;
+	}
+	
+	
 	
 	
 }
