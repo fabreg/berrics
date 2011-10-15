a71d15564aa7c62dde51c8de0a319f6d3513d432
John Hardy <john.hardy@me.com>
2011-10-14 21:40:19 -0700

    UPDATE DAILYOPS CONTROLLER TO ONLY SHOW NYJAH ON THE 15TH AND 16TH

--- theberrics.com/public_html/app/controllers/dailyops_controller.php
+++ theberrics.com/public_html/app/controllers/dailyops_controller.php
@@ -63,7 +63,7 @@ class DailyopsController extends BerricsAppController {
 			
 			$home_page = true;
 			$batb_mode = false;
-			if(date("Y-m-d") == "2011-09-10") { 
+			if(date("Y-m-d") == "2011-10-15") { 
 				
 				$batb_mode = true;
 				
