95bf1b630d79b1017b0cf9c2ac4a347096084a85
John Hardy <john.hardy@me.com>
2011-10-27 14:55:24 -0700

    update dailyops to show at least 3 posts if not then yesterday

--- theberrics.com/public_html/app/controllers/dailyops_controller.php
+++ theberrics.com/public_html/app/controllers/dailyops_controller.php
@@ -102,7 +102,7 @@ class DailyopsController extends BerricsAppController {
 		
 		
 		
-		if($home_page && count($dailyops)<2 && !$batb_mode) {
+		if($home_page && count($dailyops)<3 && !$batb_mode) {
 			
 			//let's get yesterdays posts
 			//$yesterday = date("Y-m-d",strtotime("-2 Day",strtotime($dateIn)));
