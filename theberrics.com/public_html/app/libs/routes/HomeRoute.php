26782bdd5e3becadae9a6b8d93957ea582d6c2ab
John Hardy <john.hardy@me.com>
2011-12-19 00:04:39 -0800

    fix fuckup

--- theberrics.com/public_html/app/libs/routes/HomeRoute.php
+++ theberrics.com/public_html/app/libs/routes/HomeRoute.php
@@ -12,10 +12,10 @@ class HomeRoute extends CakeRoute {
 			
 		}
 		
-		if(date("Y-m-d")=="2011-12-19") {
+		if(date("Y-m-d")=="2011-12-19" && $_SERVER['REQUEST_URI'] == "/dailyops") {
 			
-			//$params['controller'] = "younited_nations";
-			//$params['action'] = "crews";
+			$params['controller'] = "younited_nations";
+			$params['action'] = "crews";
 			
 		}
 		
