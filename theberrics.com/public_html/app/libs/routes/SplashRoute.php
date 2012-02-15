694e59746a77105bc505806e1c33efdf0169544e
John Hardy <john.hardy@me.com>
2012-02-15 08:28:13 -0800

    update splash route to use cole dc splash

--- theberrics.com/public_html/app/libs/routes/SplashRoute.php
+++ theberrics.com/public_html/app/libs/routes/SplashRoute.php
@@ -23,7 +23,7 @@ class SplashRoute extends CakeRoute {
 		//$news = true;
 		
 		$params['controller'] = "splash";
-		$params['action'] = "random";
+		$params['action'] = "dc_cole";
 		
 		if($news) {
 			
