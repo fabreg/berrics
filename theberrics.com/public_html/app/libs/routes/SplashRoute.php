da1774b64888a22fb427b1e21e2399c94b2ef1ec
John Hardy <john.hardy@me.com>
2012-02-25 09:08:40 -0800

    update the splash route

--- theberrics.com/public_html/app/libs/routes/SplashRoute.php
+++ theberrics.com/public_html/app/libs/routes/SplashRoute.php
@@ -23,7 +23,7 @@ class SplashRoute extends CakeRoute {
 		//$news = true;
 		
 		$params['controller'] = "splash";
-		$params['action'] = "lrg";
+		$params['action'] = "index";
 		
 		if($news) {
 			
