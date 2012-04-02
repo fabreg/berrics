6d49474a039f8d9bf8f592bd072752baddc1c434
John Hardy <john.hardy@me.com>
2012-04-01 21:54:16 -0700

    update splash route to be in index mode

--- theberrics.com/public_html/app/libs/routes/SplashRoute.php
+++ theberrics.com/public_html/app/libs/routes/SplashRoute.php
@@ -23,7 +23,7 @@ class SplashRoute extends CakeRoute {
 		//$news = true;
 		
 		$params['controller'] = "splash";
-		$params['action'] = "random";
+		$params['action'] = "index";
 		
 		if($news) {
 			
