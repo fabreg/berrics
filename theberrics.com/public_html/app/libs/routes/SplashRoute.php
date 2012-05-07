52428109de06ef7aac6e0a489b8c328591676c24
John Hardy <john.hardy@me.com>
2012-05-07 06:24:13 -0700

    turn on random splash

--- theberrics.com/public_html/app/libs/routes/SplashRoute.php
+++ theberrics.com/public_html/app/libs/routes/SplashRoute.php
@@ -23,7 +23,7 @@ class SplashRoute extends CakeRoute {
 		//$news = true;
 		
 		$params['controller'] = "splash";
-		$params['action'] = "index";
+		$params['action'] = "random";
 		
 		if($news) {
 			
