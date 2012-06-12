10b356a9755942f7f6afd7d15cfc6e9b6e4530d4
John Hardy <john.hardy@me.com>
2012-06-12 06:50:37 -0700

    update splash route to use sls splash

--- theberrics.com/public_html/app/libs/routes/SplashRoute.php
+++ theberrics.com/public_html/app/libs/routes/SplashRoute.php
@@ -25,7 +25,7 @@ class SplashRoute extends CakeRoute {
 		//$news = true;
 		
 		$params['controller'] = "splash";
-		$params['action'] = "index";
+		$params['action'] = "sls_kc";
 		
 		if($news) {
 			
