11dfd9c7e336c4c0026ca314118290ba641e20b9
John Hardy <john.hardy@me.com>
2012-07-20 01:25:44 -0700

    add goog etl batch

---
+++ shell/goog-batch.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+
+	$dir = dirname(__FILE__);
+	
+	$total = 47;
+
+	$count = 1;
+
+	$total_rows = 3000000;
+	
+	$start = 43000000;
+	
+	while($count<=$total) {
+		
+		echo "$dir/googetl.php --total=$total_rows --start=$start";
+		echo "\n";
+		$count++;
+		$start += $total_rows;
+		
+	}
+
+?>
