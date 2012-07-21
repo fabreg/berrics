8961eda73dec009c052178a465b912926c5b136b
John Hardy <john.hardy@me.com>
2012-07-20 17:56:44 -0700

    fix batb mode on dailyops

--- theberrics.com/public_html/app/controllers/dailyops_controller.php
+++ theberrics.com/public_html/app/controllers/dailyops_controller.php
@@ -63,7 +63,7 @@ class DailyopsController extends LocalAppController {
 			
 			$home_page = true;
 			$batb_mode = false;
-			if(in_array(date("Y-m-d"),array("2012-07-20","2012-06-16"))) { 
+			if(in_array(date("Y-m-d"),array("2012-07-21","2012-06-16"))) { 
 				
 				$batb_mode = true;
 				
