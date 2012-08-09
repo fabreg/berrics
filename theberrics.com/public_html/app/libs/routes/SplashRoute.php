4a280c6420a3c74bb0aeaf838615276b1e2d23dd
John Hardy <john.hardy@me.com>
2012-08-09 11:55:55 -0700

    update splash routes

--- theberrics.com/public_html/app/libs/routes/SplashRoute.php
+++ theberrics.com/public_html/app/libs/routes/SplashRoute.php
@@ -52,7 +52,7 @@ class SplashRoute extends CakeRoute {
 			case "2012-08-04":
 					$params['action'] = "levis";
 					break;
-			case "2012-08-08":
+			case "2012-08-09":
 				$params['action'] = "index";
 				break;
 			default:
