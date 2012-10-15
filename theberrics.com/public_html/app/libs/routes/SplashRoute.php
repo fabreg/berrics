42beb31d6a31a835b631b82c460adbb442d0cbcc
John Hardy <john.hardy@me.com>
2012-10-14 21:13:26 -0700

    update splash routes to use dc youth on monday

--- theberrics.com/public_html/app/libs/routes/SplashRoute.php
+++ theberrics.com/public_html/app/libs/routes/SplashRoute.php
@@ -26,35 +26,11 @@ class SplashRoute extends CakeRoute {
 		
 		switch(date("Y-m-d")) {
 			
-			case "2012-10-03":
-				$params['action'] = "cosmic";
 			
-				break;
-				case "2012-10-06":
-					$params['action'] = "disclosure";
-						
-					break;
-			case "2012-10-01":
-				$params['action'] = "dc_felipe";
-				$params['pass'][0]=3;
-				
-				break;
-			case "2012-09-18":
-					$params['action'] = "lrg_gallo";
-					break;
-			case '2012-22-22':
-				if(time()%2) {
-					
-					$params['action'] = "dc_felipe";
-					$params['pass'][0]=2;
-					
-				} else {
-					
-					$params['action'] = "lrg_gallo";
-					
-				}
-				break;
-			case "2012-10-04":
+			case "2012-10-15":
+				$params['action'] = "dc_youth";
+			break;
+
 			default:
 				$pages = array(
 					"dc_felipe",
