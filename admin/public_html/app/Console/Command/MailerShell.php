ccd27ae3087a4d23522c473e98e44509dc720926
John Hardy <john.hardy@me.com>
2012-10-24 00:27:13 -0700

    update

--- admin/public_html/app/Console/Command/MailerShell.php
+++ admin/public_html/app/Console/Command/MailerShell.php
@@ -4,7 +4,7 @@ App::uses('CakeEmail', 'Network/Email');
 
 class MailerShell extends AppShell {
 	
-	public $uses = array("EmailMessage","User");
+	public $uses = array("EmailMessage","User","Dailyop");
 
 	
 	public function process() {
@@ -74,6 +74,63 @@ class MailerShell extends AppShell {
 		}
 
 	}
+
+public function hit_linter() {
+		
+		//grab the last 20 published posts
+		
+		$msg = "";
+		
+		$posts = $this->Dailyop->find("all",array(
+			"conditions"=>array(
+				"Dailyop.publish_date<NOW()",
+				"Dailyop.active"=>1
+			),
+			"contain"=>array(
+				"DailyopSection"
+			),
+			"limit"=>20,
+			"order"=>array("Dailyop.publish_date"=>"DESC")
+		));
+		
+		foreach($posts as $post) {
+			
+			//let's make the URL
+			
+			$url = "http://theberrics.com/".$post['DailyopSection']['uri']."/".$post['Dailyop']['uri'];
+			
+			$this->out("++++++++++++++++++++++++++++++++++");
+			$this->out("Linting Url: ".$url);
+			
+			$url_enc = urlencode($url);
+			
+			
+			$c = curl_init();
+			curl_setopt($c,CURLOPT_FOLLOWLOCATION,true);
+			curl_setopt($c,CURLOPT_RETURNTRANSFER,true);
+			curl_setopt($c,CURLOPT_URL,"https://developers.facebook.com/tools/lint/?url={$url_enc}&format=json");
+			
+			$res = curl_exec($c);
+			
+			$this->out("Response: ".$res);
+			
+			SysMsg::add(array(
+				"category"=>"Facebook",
+				"crontab"=>1,
+				"from"=>"FacebookShell",
+				"title"=>"Linting URL: ".$url,
+				"message"=>$res
+			));
+			
+			$this->out("++++++++++++++++++++++++++++++++++");
+			$this->out("Sleeping.....zzz.z.z.z.z");
+			sleep(3);
+		}
+		
+		
+		
+	}
+
 	
 	public function process_queue() {
 
