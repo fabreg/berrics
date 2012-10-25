2b82f6da089c68da5b6a46ed1215d399b01452fa
John Hardy <john.hardy@me.com>
2012-10-24 22:50:43 -0700

    update databases

--- shared/config/database.php
+++ shared/config/database.php
@@ -5,7 +5,8 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => true,
-		'host' => '10.183.200.12',
+		'host'=>'10.176.164.233',
+		//'host' => '10.183.200.12',
 		'login' => 'john',
 		'password' => '19Berrics82',
 		'database' => 'theberrics',
@@ -15,8 +16,9 @@ class DATABASE_CONFIG {
 	public $master = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => true,
-		//'host' => '10.183.200.8',
-		'host'=>'10.183.200.12',
+		//OLD MASTER//'host' => '10.183.200.8',
+		//'host'=>'10.183.200.12',
+		'host'=>'10.176.129.133',
 		'login' => 'john',
 		'password' => '19Berrics82',
 		'database' => 'theberrics',
