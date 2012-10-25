8b3a0df86664c671273ff4bae4e685b556c8d69f
John Hardy <john.hardy@me.com>
2012-10-24 17:13:42 -0700

    remove datawarehouse

--- shared/config/database.php
+++ shared/config/database.php
@@ -32,16 +32,7 @@ class DATABASE_CONFIG {
 		'database' => 'theberrics',
 		'prefix' => '',
 	);
-	
-	public $traffic = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => true,
-		'host' => '10.183.65.132',//10.181.80.72
-		'login' => 'john',
-		'password' => '19Berrics82',
-		'database' => 'theberrics_traffic',
-		'prefix' => '',
-	);
+
 
 		
 	public function __construct() {
@@ -51,8 +42,7 @@ class DATABASE_CONFIG {
 		if($uname == 'WEB2VM') {
 			
 			$this->sessions['host'] = $this->default['host'] = $this->master['host'] = '127.0.0.1';
-			
-			$this->traffic['host'] = '50.57.129.17';
+
 			
 		}	
 
