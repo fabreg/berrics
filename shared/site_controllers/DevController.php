2ce677e8f85d5ab7401ccded5298ee10d6d205be
John Hardy <john.hardy@me.com>
2012-11-05 12:18:09 -0800

    add in testing methods add video.div.js

--- shared/site_controllers/DevController.php
+++ shared/site_controllers/DevController.php
@@ -20,9 +20,46 @@ class DevController extends LocalAppController {
 
 	public function index() {
 
+		$this->loadModel('Dailyop');
+		
 		$this->layout = "version3";
 
+		$todays_date = date("Y-m-d");
 
+		$posts = $this->Dailyop->find("all",array(
+					"conditions"=>array(
+						"DATE(Dailyop.publish_date) = '{$todays_date}'",
+						"Dailyop.active"=>1,
+						"Dailyop.hidden"=>0
+					),
+					"contain"=>array(
+						"Tag"=>array(
+							"User",
+							"Brand",
+						),
+						"DailyopMediaItem"=>array(
+							"order"=>array(
+								"DailyopMediaItem.display_weight"=>"ASC"
+							),
+							"limit"=>1,
+							"MediaFile"
+						),
+						"DailyopTextItem"=>array(
+							"order"=>array(
+								"DailyopTextItem.display_weight"=>"ASC"
+							),
+							"limit"=>1,
+							"MediaFile"
+						),
+						"DailyopSection"
+					),
+					"limit"=>3,
+					"order"=>array(
+						"Dailyop.publish_date"=>"DESC"
+					)
+		));
+
+		$this->set(compact("posts"));
 
 	}
 
--- shared/site_views/Dev/index.ctp
+++ shared/site_views/Dev/index.ctp
@@ -1,1 +1,8 @@
-<div>testing</div>
+<div class="row-fluid">
+	<div class="span8 content-column">
+		asdfasdf
+	</div>
+	<div class="span4 content-column">
+		asdfasdf
+	</div>
+</div>
