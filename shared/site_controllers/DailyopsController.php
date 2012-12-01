e91c920ac5ae02e10daa7f776255c1b4e2fbc8e8
John Hardy <john.hardy@me.com>
2012-11-30 23:01:16 -0800

    update saturday to only show one post

--- shared/site_controllers/DailyopsController.php
+++ shared/site_controllers/DailyopsController.php
@@ -63,7 +63,7 @@ class DailyopsController extends LocalAppController {
 			
 			$home_page = true;
 			$batb_mode = false;
-			if(in_array(date("Y-m-d"),array("2012-11-24"))) { 
+			if(in_array(date("Y-m-d"),array("2012-12-01"))) { 
 				
 				$batb_mode = true;
 				
