0fd670ff4a4f7da4176e1befad69396d0d3dad7a
John Hardy <john.hardy@me.com>
2013-04-11 23:46:35 -0700

    update splash route to use progression on friday 12th

--- shared/site_lib/routes/SplashRoute.php
+++ shared/site_lib/routes/SplashRoute.php
@@ -17,6 +17,7 @@ class SplashRoute extends CakeRoute {
 		switch(date("Y-m-d")) {
 
 			case "2013-04-11":
+			case "2013-04-12":
 				$params['controller'] = "progression";
 				$params['action'] = "dailyops";
 				$params['splash_page'] = true;
