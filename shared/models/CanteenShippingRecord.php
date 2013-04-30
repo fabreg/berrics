e0c5320285f587431972ac7972c368119b4520bd
John Hardy <john.hardy@me.com>
2013-04-30 15:22:43 -0700

    add in batch checkout method to tester controller

--- shared/models/CanteenShippingRecord.php
+++ shared/models/CanteenShippingRecord.php
@@ -564,7 +564,7 @@ class CanteenShippingRecord extends AppModel {
 				"category"=>"USPSError",
 				"crontab"=>1,
 				"from"=>"CanteenShippingRecord",
-				"title"=>"USPS INT Shipping Error: CanteenShippingRecordID: ".$record['CanteenShippingRecord']['id'],
+				"title"=>"USPS INT Shipping Error: CanteenShippingRecordID: <a href='/canteen_shipping_records/edit/{$record['CanteenShippingRecord']['id']}' target='_blank'>".$record['CanteenShippingRecord']['id']."</a>",
 				"message"=>serialize($xml->asXml())
 			));
 			
