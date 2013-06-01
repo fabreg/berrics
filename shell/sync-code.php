14108ea61cbff014fe673ef2240eb3ed34b1274f
John Hardy <john.hardy@me.com>
2013-06-01 09:13:48 -0700

    add more servers

--- shell/sync-code.php
+++ shell/sync-code.php
@@ -38,7 +38,17 @@ $servers = array(
 			"wnode2",
 			"wnode3",
 			"wnode4",
-			"wnode5"
+			"wnode5",
+			"node1",
+			"node2",
+			"node3",
+			"node4",
+			"node5",
+			"node6",
+			"node7",
+			"node8",
+			"node9",
+			"node10"
 		);
 
 
